676f9ff3515c331cff402e56d9f127d3336097dc
jan25 <abhilashgnan@gmail.com>
2017-04-22 16:39:36 +0800

    get all util function added in dbUtil

---
+++ routes/dbUtil.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var assert = require('assert');
+
+var mongoClient = require('mongodb').MongoClient;
+var dbUrl = require('../config/app.config').mongodb.url;
+
+var COLLECTION_NAME = 'collection_basic';
+
+var util = {
+  insertOne: function (db, collection, data) {
+    return db.collection(collection).insertOne(data);
+  },
+
+  getAll: function (db, collection) {
+    var cursor = db.collection(collection).find();
+    return cursor.toArray();
+  }
+};
+
+router.get('/algorithm/:name', function (req, res, next) {
+  var name = req.params.name || 'None :(';
+  res.send('This is ' + name + ' algorithm page');
+});
+
+router.get('/getAll', function (req, res, next) {
+  var collection = COLLECTION_NAME;
+  mongoClient.connect(dbUrl, function (err, db) {
+    assert.equal(err, null);
+
+    var promise = util.getAll(db, collection);
+    promise.then(function (docs) {
+      console.log(docs);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(docs, null, 3)); // pretty json response TODO
+    }).catch(function (err) {
+      console.log(err);
+    });
+
+    db.close();
+  });
+});
+
+router.post('insert', function (req, res, next) {
+  // TODO insert function
+  res.send('TODO insert function in api');
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var assert = require('assert');
+
+// mongodb setup local
+var mongoClient = require('mongodb').MongoClient;
+var dbUrl = require('./config/app.config').mongodb.url;
+mongoClient.connect(dbUrl, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  db.close();
+});
+
+
+var index = require('./routes/index');
+var algo = require('./routes/algo');
+var dbUtil = require('./routes/dbUtil');
+
+var port = 3443;
+
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/src'))); // fix for latest angular
+
+// Body Parser MW(middleware)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api', algo);
+app.use('/dbUtil', dbUtil);
+
+app.listen(port, function(){
+    console.log('Server started on port ' + port);
+});
